517b76a4b517c6a25e127e97174a6c0ed6942c44
moghir2004 <moghir2004@gmail.com>
2018-05-11 18:04:50 +0100

    Finished !

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 var myVar = setInterval(myTimer(), 1000);
-var dt = new Date();
-function date() {
-    document
-        .getElementById("date")
-        .innerHTML = dt.toLocaleDateString();
-}
-var myVar = setInterval(myTimer, 1000);
 function myTimer() {
     var d = new Date();
     document
